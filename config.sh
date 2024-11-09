97d6fa2a8be6df916893a128a1f78aa7d255e8fe
François Lenne <francois.lenne@hotmail.fr>
2024-11-09 18:49:02 +0100

    add the code for deploying an aws lmabda function

--- config.sh
+++ config.sh
@@ -10,3 +10,10 @@ aws secretsmanager create-secret --name cinema_key --secret-string '{"cinema_key
 
 aws secretsmanager get-secret-value --secret-id cinema_key
 
+
+aws lambda create-function \
+    --function-name Lambdacinema \
+    --runtime python3.10 \
+    --role arn:aws:iam::123456789012:role/execution_role \
+    --handler lambda_function.lambda_handler \
+    --zip-file fileb://lambda_deployment.zip
